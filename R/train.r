c58dc88b65f941035e390b8e3c9a032b9bbabeb8
Lais Fonseca <fonsecaaraujolais@gmail.com>
2025-10-24 18:34:22 -0300

    atualiza_manipulacao_data_set

--- R/train.r
+++ R/train.r
@@ -1,4 +1,4 @@
-train_main <- function(args){ # ISABELA - EM DESENVOLVIMENTO - NAO ESTA FUNCIONANDO
+train_main <- function(args) { # ISABELA - EM DESENVOLVIMENTO - NAO ESTA FUNCIONANDO
     conn <- conectamock_pfv(args$input)
     v_usinas <- args$ids_usinas
     v_horizonte <- args$horizonte_dias
@@ -8,34 +8,69 @@ train_main <- function(args){ # ISABELA - EM DESENVOLVIMENTO - NAO ESTA FUNCIONA
 
     data_set <- get_dataset(args, conn, dias = 180)
 
-    data_set$irrad_prev <- associa_nwp_usina(dt_usinas = dt_usinas, dt_prev = data_set[names(data_set) != "ger_obs"])
-    data_set$irrad_prev <- interpola_previsao_nwp(data_set = data_set$irrad_prev)
-    data_set$irrad_prev <- adicionar_passo_previsao(dt_prev = data_set$irrad_prev)
-    data_set$irrad_prev <- compatibiliza_datas(data_set)
+    data_set_ger <- data_set$ger_obs
+    data_set_met <- data_set[names(data_set) != "ger_obs"]
 
+    data_set_met <- lapply(data_set_met, associa_nwp_usina, dt_usinas)
+
+    # FUNCAO QUE CHECA SE NAO HA SALTO ENTRE AS DATAS DE PREVISAO
+
+    data_set_met <- lapply(data_set_met, interpola_previsao_nwp)
+    data_set_met <- lapply(data_set_met, adicionar_passo_previsao)
+    # data_set$irrad_prev <- compatibiliza_datas(data_set)
+
+    artefatos <- lapply(v_usinas, treina_usina,
+        dt_usinas = dt_usinas,
+        dt_ger_obs = data_set_ger,
+        dt_prev = data_set_met,
+        fator_tolerancia_geracao = args$fator_tolerancia_limite_inferior_geracao,
+        fator_tolerancia_horas = args$percentual_dias_geracao
+    )
+}
+
+treina_usina <- function(
+    iu, dt_usinas, dt_ger_obs, dt_prev,
+    fator_tolerancia_geracao, fator_tolerancia_horas) {
+    # Filtra os dados de geracao e meteorologicos referentes a usina atual
+    dad_usi <- dt_usinas[id_usina == iu]
+    ger_usi <- dt_ger_obs[id_usina == iu]
+    prev_met_usi <- lapply(dt_prev, function(dt) dt[id_usina == iu])
+
+    ger_usi[, hora_min := format(data_hora_observacao, "%H:%M")]
+    prev_met_usi <- lapply(dt_prev, function(dt) {
+        dt[, hora_min := format(data_hora_previsao, "%H:%M")]
+    })
+
+    # identificacao das semi-horas com geracao solar
+    periodo_ger <- identifica_periodo_ger(dad_usi, ger_usi, fator_tolerancia_geracao, fator_tolerancia_horas)
+
+    # gera lista com as combinacoes nwp x passo de previsao x meia-hora
+    list_comb <- gera_combinacoes_modelo(v_modelos_nwp, v_horizonte, periodo_ger)
+
+    # treina o arima
+    janela_dias_modelo <- 365
+    mod_aju <- lapply(list_comb, train_arima, ger_usi, prev_met_usi, janela_dias_modelo)
 }
 
 #' Treinamento Usando O Metodo Fisico Estimado
 #'
 #' Realiza treinamento do modelo fisico estimado e salva modelo para uso futuro
-#' 
+#'
 #' @param data_set lista contendo o subset dos dados utilizados para treinamento do modelo.
-#' O subset ja deve conter os dados do periodo adequado para o treinamento. Cada item da lista 
+#' O subset ja deve conter os dados do periodo adequado para o treinamento. Cada item da lista
 #' corresponde a uma variavel usada no treinamento, tanto variavel resposta quanto explicativas.
-#' 
+#'
 #' @return modelos ajustados
-#' 
-train_fisico_estimado <- function(data_set){
-
+#'
+train_fisico_estimado <- function(data_set) {
     data_set <- lapply(data_set, function(x) x[valor == 0, valor := NA])
     data_set <- mapply(renomeia_colunas, data_set, "data_hora_observacao", "data_hora")
     data_set <- mapply(renomeia_colunas, data_set, "data_hora_previsao", "data_hora")
     data_set <- lapply(data_set, function(x) x[, hora := format(data_hora, "%H:%M")])
     vetor_horas <- unique(data_set$ger_obs$hora) # ISABELA - DEPOIS SUBSTITUIR PELO PERIODO DE GERACAO IDENTIFICADO PARA CADA USINA
-   
 
-    for (h in vetor_horas){
 
+    for (h in vetor_horas) {
         list_y <- data_set[names(data_set) == "ger_obs"] # ISABELA - VERIFICAR SE TEM FORMA MELHOR DE FAZER
         list_x <- data_set[names(data_set) != "ger_obs"]
 
@@ -51,42 +86,86 @@ train_fisico_estimado <- function(data_set){
 
 #' Aplica Regressao Linear
 #'
-#' Funcao que aplica regressao linear a um conjunto de dados e salva o 
-#' 
+#' Funcao que aplica regressao linear a um conjunto de dados e salva o
+#'
 #' @param dt_y ´data.table´ contendo a variavel resposta. Exemplo: geracao observada
 #' @param dt_x ´data.table´ contendo a(s) variavel(is) explicativas. Exemplo: irradiancia, umidade e temperatura
 #' As observacoes contidas nos data.tables contendo as variaveis ja devem estar em posicoes compativeis
-#' 
+#'
 #' @return modelos ajustados
-#' 
-aplica_regressao_linear <- function(dt_y, dt_x){
-
-    dados <- cbind(dt_y, dt_x)    
+#'
+aplica_regressao_linear <- function(dt_y, dt_x) {
+    dados <- cbind(dt_y, dt_x)
     resposta <- names(dt_y)
     preditoras <- names(dt_x)
 
-    formula <- paste(resposta, "~", paste(preditoras, collapse = "+" ))
+    formula <- paste(resposta, "~", paste(preditoras, collapse = "+"))
     formula_objeto <- as.formula(formula)
-    
+
     modelo <- lm(formula_objeto, data = dados)
     return(modelo)
 }
 
-train_arima <- function(pars, ger_usi, irr_usi, janela_dias) {
-
+train_arima <- function(pars, ger_usi, prev_met_usi, janela_dias) {
     ger_usi_filt <- copy(ger_usi)
-    irr_usi_filt <- copy(irr_usi)
+    prev_met_usi_filt <- copy(prev_met_usi)
 
     ger_usi_filt <- ger_usi_filt[hora_min == pars$hora_min]
-    
-    irr_usi_filt <- irr_usi_filt[
+
+    prev_met_usi_filt <- lapply(prev_met_usi_filt, function(dt) {
+        dt[
             id_modelo_nwp == pars$id_modelo_nwp &
-            passo_prev == pars$horiz_prev &
-            hora_min == pars$hora_min
+                passo_prev == pars$horiz_prev &
+                hora_min == pars$hora_min
         ]
+    })
 
-    # comeca auto.arima
+    prev_met_usi_filt <- rbindlist(prev_met_usi_filt, idcol = "variavel_meteoro")
+    
+    id_cols <- setdiff(names(prev_met_usi_filt), c("valor", "variavel_meteoro"))
 
+    prev_usi_wide <- dcast(
+        prev_met_usi_filt,
+        formula = paste(paste(id_cols, collapse = " + "), "~ variavel_meteoro"),
+        value.var = "valor"
+    )
+
+    # une geracao com a variavel meteorologica limitando pela data fim do observado
+
+    # seleciona janela dos dados para treinamento
+    dt_treino <- seleciona_janela(ger_usi_filt, prev_met_usi_filt, janela_dias_treinamento = 300)
+
+    # avalia numero de conjuntos ger x irr x temp x umid
+    if (dados_suficientes(dt_treino, num_min_dados = 5) == TRUE) {
+        # ajusta modelo dummy
+        nlmod0 <- ajusta_dummy(dt_treino)
+
+        # normaliza as variaveis necessarias para o ajuste
+        norm_resultado <- normaliza_variaveis(dt_treino)
+        dt_treino_norm <- norm_resultado$dados
+        stats_norm <- norm_resultado$stats
+
+        # ajusta ARIMA
+        nlmod1 <- ajusta_arima(dt_treino_norm, nlmod0)
+    }
+    # proximas funcoes sao aplicadas somente se a condicao for satisfeita
+    # numero minimo pode ser parametro de entrada
+
+
+
+
+
+
+    # cria diferenciacao para alguns horarios dias para que o ajuste
+    # seja apenas ger x irr
+
+    # ajusta arimax
+
+    # calcular erro medio in-sample
+
+    # seleciona do modelo com base no desvio e aicc
+
+    # retorna modelos
 }
 
 # AUXILIARES ---------------------------------------------------------------------------------------
@@ -94,14 +173,13 @@ train_arima <- function(pars, ger_usi, irr_usi, janela_dias) {
 #' Cria Dataset Dos Dados Para Treino dos Modelos
 #'
 #' Cria dataset dos dados usados no treinamento dos modelos de previsao de acordo configuracao especificada
-#' 
+#'
 #' @param args Lista de argumentos necessarios para criacao dos datasets
 #' @param conn Objeto de conexao com banco de dados
-#' 
+#'
 #' @return lista contendo o dataset
-#' 
-get_dataset <- function(args, conn, dias){
-    
+#'
+get_dataset <- function(args, conn, dias) {
     janela <- paste0(args$data_referencia - dias, "/", args$data_referencia)
 
     ger_obs <- get_geracao_observada(conn, id_usina = args$ids_usinas)
@@ -113,25 +191,24 @@ get_dataset <- function(args, conn, dias){
     return(out)
 }
 
-preenche_lacunas_previsao <- function(data_set){ # ISABELA - EM DESENVOLVIMENTO - NAO ESTA FUNCIONANDO
+preenche_lacunas_previsao <- function(data_set) { # ISABELA - EM DESENVOLVIMENTO - NAO ESTA FUNCIONANDO
 
     datas_rodadas <- unique(data_set$data_hora_rodada)
-
 }
 
-compatibiliza_datas <- function(data_set){ # ISABELA - EM DESENVOLVIMENTO - NAO ESTA FUNCIONANDO
+compatibiliza_datas <- function(data_set) { # ISABELA - EM DESENVOLVIMENTO - NAO ESTA FUNCIONANDO
 
-    ger_obs <- data_set$ger_obs  
+    ger_obs <- data_set$ger_obs
     ger_obs_colorder <- names(ger_obs)
 
     irrad_prev <- data_set$irrad_prev
     irrad_prev_colorder <- names(irrad_prev)
 
-    data_ini <- max(min(ger_obs$data_hora_observacao),min(irrad_prev$data_hora_rodada))
-    data_fim <- min(max(ger_obs$data_hora_observacao),max(irrad_prev$data_hora_previsao))
+    data_ini <- max(min(ger_obs$data_hora_observacao), min(irrad_prev$data_hora_rodada))
+    data_fim <- min(max(ger_obs$data_hora_observacao), max(irrad_prev$data_hora_previsao))
     janela <- seq.POSIXt(from = data_ini, to = data_fim, by = "30 min")
 
-    dt_janela <- data.table(data_hora_observacao = janela)    
+    dt_janela <- data.table(data_hora_observacao = janela)
     ger_obs <- merge(ger_obs, dt_janela, by = "data_hora_observacao", all.y = TRUE)
     setcolorder(ger_obs, ger_obs_colorder)
 
@@ -146,7 +223,88 @@ compatibiliza_datas <- function(data_set){ # ISABELA - EM DESENVOLVIMENTO - NAO 
     return(data_set)
 }
 
-renomeia_colunas <- function(dt, nome_atual, nome_novo){
+renomeia_colunas <- function(dt, nome_atual, nome_novo) {
     names(dt)[names(dt) == nome_atual] <- nome_novo
     return(dt)
+}
+
+#' Seleciona janela dos dados para treinamento
+#'
+#' @param dt_ger `data.table` com data_hora_observacao, geracao
+#' @param janela_dias_treinamento numero de dias utilizados
+#' para treinamento
+#'
+#' @return subset do data.table filtrado
+
+seleciona_janela <- function(dt, janela_dias_treinamento) {
+    dt <- copy(dt)
+    dt[, data := as.Date(data_hora_observacao)]
+
+    # considerar apenas as ultimas `janela_dias` datas
+    ultimas_datas <- head(sort(unique(dt$data), decreasing = TRUE), janela_dias_treinamento)
+    dt <- dt[data %in% ultimas_datas]
+
+    return(dt)
+}
+
+#' Verifica se ha dados suficientes para o ajuste
+#'
+#' @param dt `data.table` com data_hora, geracao, irradiancia,
+#' temperatura e umidade
+#' @param num_min_dados numero minimo de registros validos simultaneos
+#'
+#' @return TRUE se houver dados validos, FALSE caso contrario
+
+dados_suficientes <- function(dt, num_min_dados) {
+    n_validos <- nrow(na.omit(dt[, .(ger, irr, temp, umid)]))
+    if (n_validos < num_min_dados) {
+        return(FALSE)
+    }
+    TRUE
+}
+
+#' Cria um modelo ARIMA dummy
+#'
+#' @param dt  `data.table` com data_hora, geracao, irradiancia,
+#' temperatura e umidade do periodo de treinamento selecionado
+#' @return modelo ARIMA neutro
+
+ajusta_dummy <- function(dt) {
+    auto.arima(rep(0, nrow(dt)), allowdrift = FALSE, allowmean = FALSE)
+}
+
+#' Normaliza variaveis de entrada
+#'
+#' @param dt `data.table` com data_hora, geracao, irradiancia,
+#' temperatura e umidade do periodo de treinamento selecionado
+#'
+#' @return lista com:
+#' \itemize {
+#'  \item \code{dados} - data.table com variaveis normalizadas
+#'  \item \code{stats} - medias e desvios padroes de cada variavel
+#' }
+
+normaliza_variaveis <- function(dt) {
+    stats <- dt[, .(
+        ger_med = mean(ger, na.rm = TRUE), ger_sd = sd(ger, na.rm = T),
+        irr_med = mean(irr, na.rm = TRUE), irr_sd = sd(irr, na.rm = T)
+    )]
+
+    dt[, ger_norm := (ger - stats$ger_med) / stats$ger_sd]
+    dt[, irr_norm := (irr - stats$irr_med) / stats$irr_sd]
+
+    list(dados = dt, stats = stats)
+}
+
+#' Ajusta modelo ARIMA simples
+#'
+#' @param dt data.table com variavel \code{ger_norm}.
+#' @param nlmod0 modelo dummy utilizado em caso de erro.
+#'
+#' @return modelo ajustado do tipo \code{Arima}.
+ajusta_arima <- function(dt, nlmod0) {
+    tryCatch(
+        auto.arima(dt$ger_N, allowdrift = FALSE, allowmean = FALSE),
+        error = function(e) nlmod0
+    )
 }
--- R/utils.r
+++ R/utils.r
@@ -3,16 +3,16 @@
 #' Obtem as previsoes dos modelos NWP para as usinas simuladas. Para isso, realiza a associacao das coordenadas das
 #' usinas com quadricula correspondente do modelo NWP.
 #' E escolhida a quadricula que apresenta menor distancia euclidiana entre seu centroide e a coordenada da usina.
-#' 
+#'
 #' @param dt_usinas ´data.table´ contendo os dados cadastrais das usinas
 #' @param dt_prev ´data.table´ contendo os dados das variaveis previstas do modelo NWP
-#' 
+#'
 #' @return ´data.table´ das usinas simuladas e as respectivas variaveis previstas do modelo NWP
-#' 
+#'
 #' OBSERVACAO ISABELA: Funcao aproveitada do MH, troquei o nome do argumento dt_irrad_prev para dt_prev para deixar generico
 
 # Função usando distância euclidiana
-associa_nwp_usina <- function(dt_usinas, dt_prev) {
+associa_nwp_usina <- function(dt_prev, dt_usinas) {
     # Coordenadas únicas da previsão
     coord_prev <- unique(dt_prev[, .(latitude, longitude)])
 
@@ -31,7 +31,7 @@ associa_nwp_usina <- function(dt_usinas, dt_prev) {
 
         # Filtra os dados da previsão para essa coordenada
         dt_filt <- dt_prev[latitude == coord_mais_proxima$latitude &
-                longitude == coord_mais_proxima$longitude]
+            longitude == coord_mais_proxima$longitude]
 
         # Adiciona o id_usina
         dt_filt[, id_usina := usina$id_usina]
@@ -57,14 +57,14 @@ associa_nwp_usina <- function(dt_usinas, dt_prev) {
 #' Obtem as previsoes dos modelos NWP para as usinas simuladas. Para isso, realiza a associacao das coordenadas das
 #' usinas com quadricula correspondente do modelo NWP.
 #' E escolhida a quadricula que apresenta menor distancia euclidiana entre seu centroide e a coordenada da usina.
-#' 
+#'
 #' @param dt_usinas ´data.table´ contendo os dados cadastrais das usinas
 #' @param dt_prev ´data.table´ contendo os dados das variaveis previstas do modelo NWP
-#' 
+#'
 #' @return ´data.table´ das usinas simuladas e as respectivas variaveis previstas do modelo NWP
-#' 
+#'
 #' OBSERVACAO ISABELA: Funcao aproveitada do MH, troquei o nome do argumento dt_irrad_prev para dt_prev para deixar generico
-#' 
+#'
 adicionar_passo_previsao <- function(dt_prev) {
     # Garante que as colunas são do tipo POSIXct
     dt_prev[, data_hora_rodada := as.POSIXct(data_hora_rodada)]
@@ -80,54 +80,62 @@ adicionar_passo_previsao <- function(dt_prev) {
 }
 
 #' Interpola Dados De Previsao NWP
-#' 
+#'
 #' Realiza interpolação dos dados previstos dos modelos NWP em intervalos semi-horarios
-#' 
+#'
 #' @param data_set ´data.table´ com os dados de previsao NWP a serem interpolados
-#' 
+#'
 #' @return ´data.table´ com dados previstos interpolados
-#' 
-interpola_previsao_nwp <- function(data_set){
+#'
+interpola_previsao_nwp <- function(data_set_met) {
+    colorder <- names(data_set_met)
+    data_hora_interpolacao <- cria_sequencia_datas(dt = data_set_met, discretizacao = 30)
+    data_set_discretizacao <- merge(data_hora_interpolacao, data_set_met, by = c("data_hora_rodada", "data_hora_previsao", "id_modelo_nwp", "id_usina"), all.x = TRUE)
+    data_set_interpolado <- data_set_discretizacao[, valor := interpola_serie_temporal(valor),
+        by = .(id_modelo_nwp, id_usina, data_hora_rodada)
+    ]
+    cols_propagar <- setdiff(names(data_set_interpolado), c(names(data_hora_interpolacao), "valor"))
 
-    colorder <- names(data_set)
-    data_hora_interpolacao <- cria_sequencia_datas(dt = data_set, discretizacao = 30)    
-    data_set_discretizacao <- merge(data_hora_interpolacao, data_set, by = c("data_hora_rodada","data_hora_previsao","id_modelo_nwp","id_usina"), all.x = TRUE)
-    data_set_interpolado <- data_set_discretizacao[, valor := intepola_serie_temporal(data_set_discretizacao$valor)]
+    for (col in cols_propagar) {
+        data_set_interpolado[, (col) := nafill(get(col), type = "locf"),
+            by = .(id_modelo_nwp, id_usina, data_hora_rodada)
+        ]
+    }
+
     setcolorder(data_set_interpolado, colorder)
 
     return(data_set_interpolado)
 }
 
 #' Cria Sequencia De Datas
-#' 
+#'
 #' Identifica as datas de inicio e fim das series temporais
 #' e cria dt com a sequencia completa de datas
-cria_sequencia_datas <- function(dt, discretizacao){
-
+cria_sequencia_datas <- function(dt, discretizacao) {
     dt_data_inicio <- dt[, .(data_inicio = min(data_hora_previsao)), by = .(id_modelo_nwp, id_usina, data_hora_rodada)]
     dt_data_fim <- dt[, .(data_fim = max(data_hora_previsao)), by = .(id_modelo_nwp, id_usina, data_hora_rodada)]
-    dt_datas_inicio_fim <- merge(dt_data_inicio, dt_data_fim, by = c("id_modelo_nwp","id_usina", "data_hora_rodada"))
-    dt_sequencia_datas <- dt_datas_inicio_fim[ , .(data_hora_previsao = seq(data_inicio, data_fim, by = paste0(discretizacao," min"))), by = .(id_modelo_nwp, id_usina, data_hora_rodada)]
-    
+    dt_datas_inicio_fim <- merge(dt_data_inicio, dt_data_fim, by = c("id_modelo_nwp", "id_usina", "data_hora_rodada"))
+    dt_sequencia_datas <- dt_datas_inicio_fim[, .(data_hora_previsao = seq(data_inicio, data_fim, by = paste0(discretizacao, " min"))), by = .(id_modelo_nwp, id_usina, data_hora_rodada)]
+
     return(dt_sequencia_datas)
 }
 
 #' Interpola Serie Temporal
-#' 
+#'
 #' Identifica as lacunas da serie temporal e preenche realizando interpolacao linear
-intepola_serie_temporal <- function(dt){
+interpola_serie_temporal <- function(dt) {
     dt_interpolado <- na.approx(dt)
     return(dt_interpolado)
-} 
+}
 
 #' Identifica periodo do dia com geracao solar
-#' 
+#'
 #' @description
 #' A funcao identifica horarios ("HH:MM") em que ha geracao
 #' ao longo de um ano.
 #' Util para definir periodo em que havera treinamento de modelos
 #' e previsao de geracao solar
-#' 
+#'
 #' @param dad_usina data.table contendo pelo menos:
 #' \itemize {
 #' \item id_usina
@@ -139,17 +147,17 @@ intepola_serie_temporal <- function(dt){
 #' \item valor
 #' }
 #' @param fator_tol_ger
-#' fator minimo da capacidade instalada da usina para considerar 
+#' fator minimo da capacidade instalada da usina para considerar
 #' que houve geracao
 #' @param janela_dias
 #' numero de dias passados a considerar na analise
 #' @param fator_tol_horas
 #' fator minimo dos dias analisados com geracao
-#' 
-#' @return 
+#'
+#' @return
 #' vetor de characteres com as meias-horas ("HH:MM")
-#' 
-#' @details 
+#'
+#' @details
 #' A funcao segue as etapas:
 #' \itemize {
 #'   \item extrai a meia-hora, hora_min, de cada observacao
@@ -159,38 +167,37 @@ intepola_serie_temporal <- function(dt){
 #' }
 
 identifica_periodo_ger <- function(dad_usina, ger_usi, fator_tol_ger, fator_tol_horas) {
-  
-  janela_dias <- 300
-  dt <- copy(ger_usi)
-  
-  dt[, hora_min := format(data_hora_observacao, "%H:%M")]
-  dt[, data := as.Date(data_hora_observacao)]
-  
-  # considerar apenas as ultimas `janela_dias` datas
-  ultimas_datas <- head(sort(unique(dt$data), decreasing = TRUE), janela_dias)
-  dt <- dt[data %in% ultimas_datas]
-  
-  # contar em quantos dias houve geracao acima do limiar para cada hora:minuto
-  resumo <- dt[
-    valor > fator_tol_ger * dad_usi$capacidade_instalada_MW,
-    .(dias_com_geracao = uniqueN(data)),
-    by = hora_min
-  ]
-  
-  dias_minimos <- fator_tol_horas * length(ultimas_datas)
-  horas_validas <- resumo[dias_com_geracao >= dias_minimos, hora_min]
-  
-  horas_validas <- sort(horas_validas)
-  
-  return(horas_validas)
+    janela_dias <- 300
+    dt <- copy(ger_usi)
+
+    dt[, hora_min := format(data_hora_observacao, "%H:%M")]
+    dt[, data := as.Date(data_hora_observacao)]
+
+    # considerar apenas as ultimas `janela_dias` datas
+    ultimas_datas <- head(sort(unique(dt$data), decreasing = TRUE), janela_dias)
+    dt <- dt[data %in% ultimas_datas]
+
+    # contar em quantos dias houve geracao acima do limiar para cada hora:minuto
+    resumo <- dt[
+        valor > fator_tol_ger * dad_usi$capacidade_instalada_MW,
+        .(dias_com_geracao = uniqueN(data)),
+        by = hora_min
+    ]
+
+    dias_minimos <- fator_tol_horas * length(ultimas_datas)
+    horas_validas <- resumo[dias_com_geracao >= dias_minimos, hora_min]
+
+    horas_validas <- sort(horas_validas)
+
+    return(horas_validas)
 }
 
 #' Gera combinacoes entre nwp, passo de previsao e meia-hora
-#' 
-#' @description 
-#' Cria uma lista com todas as combinacoes possiveis entre modelos meteorologicos, 
+#'
+#' @description
+#' Cria uma lista com todas as combinacoes possiveis entre modelos meteorologicos,
 #' passos de previsao e meias-horas
-#' 
+#'
 #' @param v_modelos_nwp vetor com os nomes dos modelos meteorologicos, ex: c("GFS")
 #' @param v_horizonte vetor de caracteres com os passos de previsao, ex: c("D+0", "D+1")
 #' @param periodo_ger vetor de horarios no formato HH:MM, ex: c("12:00", "12:30")
@@ -201,19 +208,18 @@ identifica_periodo_ger <- function(dad_usina, ger_usi, fator_tol_ger, fator_tol_
 #' \item horiz_prev - passo de previsao
 #' \item periodo_ger - meias-horas com geracao solar
 #' }
-#' 
-#' @examples 
+#'
+#' @examples
 #' v_nwp <- c("GFS", "ECMWF")
 #' v_horiz <- c("D+0", "D+1")
 #' v_hor_ger <- c("12:00", "12:30")
 #' gera_combinacoes_modelo(v_nwp, v_horiz, v_hor_ger)
-
 gera_combinacoes_modelo <- function(v_modelos_nwp, v_horizonte, periodo_ger) {
     comb <- CJ(
         id_modelo_nwp = unlist(v_modelos_nwp),
         horiz_prev = v_horizonte,
         hora_min = periodo_ger
-        )
+    )
 
     lista_comb <- split(comb, seq_len(nrow(comb)))
     lista_comb <- lapply(lista_comb, as.list)
@@ -222,34 +228,34 @@ gera_combinacoes_modelo <- function(v_modelos_nwp, v_horizonte, periodo_ger) {
 }
 
 #' Filtra dados meteorologicos para cada combinacao nwp x passo de previsao x meia-hora
-#' 
+#'
 #' @description
 #' Para cada trio modelo meteorologico x passo de previsao x meias-horas,
 #' e filtrado o conjunto de dados de interesse do data.table original
-#' 
+#'
 #' @param irr_usi `data.table` pelo menos com as colunas:
 #' \itemize id_modelo_nwp - modelo meteorologico
 #' \item horiz_prev - passo de previsao
 #' \item hora_min - meias-horas com geracao solar (formato HH:MM)
 #' \item valor - irradiancia observada
-#' 
+#'
 #' @param list_comb lista de listas, em que cada elemento contem:
 #' \itemize {
 #'   \item id_modelo_nwp
 #'   \item horiz_prev
 #'   \item hora_min
 #' }
-#' 
+#'
 #' @return `data.table` com os dados meteorologicos filtrados
 
 filtra_dado_por_combinacao <- function(irr_usi, list_comb) {
-    irr_usi_filt = copy(irr_usi)
-    
+    irr_usi_filt <- copy(irr_usi)
+
     lapply(list_comb, function(pars) {
         irr_usi_filt[
             id_modelo_nwp == pars$id_modelo_nwp &
-            passo_prev == pars$horiz_prev &
-            hora_min == pars$hora_min
+                passo_prev == pars$horiz_prev &
+                hora_min == pars$hora_min
         ]
     })
 }
